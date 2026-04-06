cfa58f688f0a225f2a5354003acdccc154ac6784
Mohammed <hastreams6@gmail.com>
2026-04-06 20:23:32 +0300

    removed unneccesary name

--- Backend/Database/Schemas/accountSchema.js
+++ Backend/Database/Schemas/accountSchema.js
@@ -1,4 +1,4 @@
-const { Schema, model, default: mongoose, version } = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
 
 function formatCred(ret) {
   return {
